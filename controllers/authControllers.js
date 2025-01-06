c08f1f35ba8e077ee3ca9cd2382a401306382dbe
tuanan <pantoan288@gmail.com>
2025-01-06 23:37:34 +0700

    Add Token

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/userModel');
 const { responseReturn } = require('../utils/response');
 const bcrypt = require('bcrypt');
+const { createToken } = require('../utils/tokenCreate');
 
 class authControllers {
     user_login = async (req, res) => {
@@ -9,8 +10,18 @@ class authControllers {
             const user = await userModel.findOne({email}).select('+password');
             if (user) {
                 const match = await bcrypt.compare(password, user.password);
-                console.log(match);
-                
+                // console.log(match);
+                if (match) {
+                    const token = await createToken({
+                        id: user._id,
+                        role: user.role
+                    });
+                    res.cookie('accessToken', token, {
+                        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)})
+                    responseReturn(res, 200, {token,message: 'Login success!'})
+                } else {
+                    responseReturn(res, 404, {error: 'Password is incorrect!'})
+                }
             } else {
                 responseReturn(res, 404, {error: 'User not found!'})
             }
